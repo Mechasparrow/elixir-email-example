f1803a552cd716445c1202c0ba3fc58649e878be
marioluigiman <mikipux7@gmail.com>
2016-06-28 11:03:34 -0500

    updated email_test script

--- lib/email_test.ex
+++ lib/email_test.ex
@@ -2,8 +2,9 @@ defmodule EmailPlay do
   import Bamboo.Email
   alias EmailPlay.Mailer, as: Mailer
   def send_test_email do
+    address = Application.get_env(:email_test, EmailPlay.Mailer)[:username]
     new_email(
-      from: "mikipux7@gmail.com",
+      from: address,
       to: "mikipux7@gmail.com",
       subject: "welcome",
       html_body: "<strong> Thanks for joinging our service</strong>",
